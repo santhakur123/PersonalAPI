10aec4cd113319fde0fe1a870f8c537438e478aa
WEB DEV <hsthakur644@gmail.com>
2025-08-31 22:47:41 +0530

    Configured frontend to use Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
 
   
-   const API_URL = "http://localhost:5000";
+   const API_URL = "https://personalapi-backend.onrender.com";
 
   useEffect(() => {
     axios.get(`${API_URL}/profile`).then((res) => setProfile(res.data));
